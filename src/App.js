b62210bf9243ee8d73970227118080f0ced0ee93
enderson <enderson19952009@gmail.com>
2019-08-14 10:54:35 -0400

    taildwind installation finish
    Instructions in this link https://tailwindcss.com/course/setting-up-tailwind-and-postcss/

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
         >
           Learn React
         </a>
+          <p>Hola</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+             Button
+          </button>
       </header>
     </div>
   );
