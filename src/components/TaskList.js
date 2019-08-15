12ce49d2f5263b7ac6b83654a712053c925c5118
Enderson <enderson19952009@gmail.com>
2019-08-14 23:00:09 -0400

    responsive component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,16 +3,16 @@ import React from "react";
 class TaskList extends React.Component {
     render (){
         return(
-            <div class="flex">
-            <div class="flex-auto block bg-gray-700 inset-y-0.left-0 px-4 pb-4 border-solid border-4 text-rigth border-solid p-1 mx-auto mt-2 w-full max-w-lg">
-                <p class="text-left px-4 text-white">List :</p>
-                <div class="block bg-gray-200 py-12 pb-2">
+            <div class="block lg:flex">
+            <div class="lg:flex-auto block bg-gray-700 px-2 lg:px-4 pb-4 border-solid border-4 text-rigth border-solid p-1 mx-auto mt-2 w-full max-w-lg">
+                <p class="text-left px-4 text-white">Task list :</p>
+                <div class="block bg-gray-200 py-12">
                     lista de tareas
                 </div>
             </div>
-            <div class="flex-auto block bg-gray-700 inset-y-0.left-0 px-4 pb-4 border-solid border-4 text-rigth border-solid p-1 mx-auto mt-2 w-full max-w-lg">
-                <p class="text-left px-4 text-white">List :</p>
-                <div class="block bg-gray-200 py-12 pb-2">
+            <div class="lg:flex-auto block bg-gray-700 px-2 lg:px-4 pb-4 border-solid border-4 text-rigth border-solid p-1 mx-auto mt-2 w-full max-w-lg">
+                <p class="text-left px-4 text-white">Task done :</p>
+                <div class="block bg-gray-200 py-12">
                     tareas hechas
                 </div>
             </div>
