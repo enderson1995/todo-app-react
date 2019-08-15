89be3f9d98d7bf637526754c806f717af4769675
Enderson <enderson19952009@gmail.com>
2019-08-14 21:26:36 -0400

    make CreateTask responsive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header.js';
 import CreateTask from './components/CreateTask.js';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
@@ -9,9 +10,10 @@ function App() {
         <Header />
       <header>
       <div>
-        <h1 class="font-hairline text-5xl text-center py-3 text-gray-200">TODO G-O</h1>
+        <h1 class="font-hairline text-3xl lg:text-5xl xl:text-5xl text-center py-3 text-gray-800">TODO G-O</h1>
       </div>
       <CreateTask />
+      <TaskList />
         <p >
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/TaskList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TaskList extends React.Component {
+    render (){
+        return(
+            <div>lista</div>
+        );
+    }
+}
+
+export default TaskList
