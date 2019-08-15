055e28c4789b7d69148a1f9b4b3ec6b8e221a7bb
Enderson <enderson19952009@gmail.com>
2019-08-14 22:42:44 -0400

    make components

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,9 +3,25 @@ import React from "react";
 class TaskList extends React.Component {
     render (){
         return(
-            <div>lista</div>
+            <div class="flex">
+            <div class="flex-auto block bg-gray-700 inset-y-0.left-0 px-4 pb-4 border-solid border-4 text-rigth border-solid p-1 mx-auto mt-2 w-full max-w-lg">
+                <p class="text-left px-4 text-white">List :</p>
+                <div class="block bg-gray-200 py-12 pb-2">
+                    lista de tareas
+                </div>
+            </div>
+            <div class="flex-auto block bg-gray-700 inset-y-0.left-0 px-4 pb-4 border-solid border-4 text-rigth border-solid p-1 mx-auto mt-2 w-full max-w-lg">
+                <p class="text-left px-4 text-white">List :</p>
+                <div class="block bg-gray-200 py-12 pb-2">
+                    tareas hechas
+                </div>
+            </div>
+            </div>
         );
     }
 }
+
+
+
 
 export default TaskList
