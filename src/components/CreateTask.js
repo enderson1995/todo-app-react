268ecab22fc3c5d0f4972613536735c82014040c
enderson <enderson19952009@gmail.com>
2019-08-15 16:22:35 -0400

    better header

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+
 class CreateTask extends React.Component {
+    handleClick(prueba){
+     
+      console.log(prueba)
+    }
 
     render() {
+      let prueba='jajaja';
       return(
         <div class="block border-solid p-1 mx-auto mt-2 w-full max-w-lg text-black">
           <span class="block text-white text-center bg-gray-700 px-4 py-2">Create a new task</span>
@@ -10,8 +16,8 @@ class CreateTask extends React.Component {
            <input class="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300" placeholder="write your new task">
             
           </input>
-          <button class="bg-teal-500 px-6 border-soild border-4 border-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Create task
+          <button onClick={(prueba)=>this.handleClick(prueba)} class="bg-teal-500 px-6 border-soild border-4 border-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Create task {prueba}
           </button>
         </div>
         </div>
