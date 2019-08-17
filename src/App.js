abb75563309baa36f22f943099f4565978123ebd
Enderson <enderson19952009@gmail.com>
2019-08-16 21:21:15 -0400

    gitflow-feature-stash: restructure
    past information father to son

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import Header from './components/Header.js';
 import CreateTask from './components/CreateTask.js';
 import TaskList from './components/TaskList';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={value:'hehehe'};
+    this.task={
+      text : '',
+      edit : false,
+      done : false,
+    };
+  }
+  handleClick(e){
+    alert('click')
+  }
+  handleChange(e){
+    this.setState({});
+  }
+  render(){
   return (
     <div className="App" >
         <Header />
       <div>
         <h1 class="font-hairline text-3xl lg:text-5xl xl:text-5xl text-center py-3 text-gray-800">TODO G-O</h1>
       </div>
-      <CreateTask />
+      <CreateTask text={this.state.value} handleClick={this.handleClick} handleChange={this.handleChange} />
       <TaskList />
+      {this.state.value}
     </div>
-  );
+  );}
 }
 
 export default App;
--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -2,39 +2,22 @@ import React from 'react';
 
 
 class CreateTask extends React.Component {
-    constructor (props){
-      super(props);
-      this.state={value: ''};
-      this.task = {
-        items: [],
-        text : 'jeje'
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick(e){   
-      this.setState({value: e.target.value}); 
-      this.task.text=this.state.value
-    }
-
-    handleChange(e) {
-      this.setState({value: e.target.value});
-    }
-
     render() {
-      let prueba='jajaja';
       return(
         <div class="block border-solid p-1 mx-auto mt-2 w-full max-w-lg text-black">
           <span class="block text-white text-center bg-gray-700 px-4 py-2">Create a new task</span>
           <div class ="bg-gray-700 lg:px-4 xl:px-4 md:px-2 border-soild border-4 border-gray-700 ">
-           <input id='new-task' type='text' value={this.state.value} onChange={this.handleChange} class="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300" placeholder="write your new task">
+           <input 
+            id='new-task'
+            value={this.props.text} 
+            onChange={this.props.handleChange} 
+            type='text' class="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300" placeholder="write your new task">
           </input>
-          <button onClick={this.handleClick} value={this.state.value} class="bg-teal-500 px-6 border-soild border-4 border-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button onClick={this.props.handleClick} class="bg-teal-500 px-6 border-soild border-4 border-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Create task 
           </button>
-       
+        {this.props.text}
         </div>
-        {this.task.text}
         </div>
       );
     } 
